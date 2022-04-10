972d8c0070a099cdb31a6d9ae221d2670e3f9594
Misha Sinitcyn <mishasin11@gmail.com>
2022-04-10 15:16:50 -0700

    outlined main runtime structure

--- q3.py
+++ q3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def andOperator(address, netmask):
     return (bin(address & netmask).replace('b', ''))
 
@@ -24,33 +26,63 @@ def addressToBinary(address):
 def netmaskInt(netmask): 
     return sum(bin(int(x)).count('1') for x in netmask.split('.'))
 
+def getNetmaskFromRow(row):
+    return netmaskInt(row[2])
+
+def sortTable(table):
+    return sorted(table, key=getNetmaskFromRow)
+
 def route(address, netmask):
-    #Implement forwarding algorithm
     destination_address = applyMask(address, netmask)
     #Use the metric (metric field - either 1 or 0)
-    next_hop_address = 'address'
+    next_hop_address = '{address}'
     PORT = 3000
-    #Output the following
-    print("The destination IP address is", destination_address)
-    print("The next hop IP address is", next_hop_address)
-    print("The port the packet will leave through is", PORT)
 
+    print("\tThe destination IP address is", destination_address)
+    print("\tThe next hop IP address is", next_hop_address)
+    print("\tThe port the packet will leave through is", PORT)
+    print('')
 
+def findRoute(address, routingTable):
+    #Search routing table and find best route
+    addressBitMask = netmaskInt(address)
+    for x in range(len(routingTable)):
+        if netmaskInt(routingTable[x][2]) == addressBitMask:
+            return routingTable[x][2]
+
+    return 'find route'
 
 def routerStart():
     #Read routing table
-    #Sort routing table
-    #Print routing table
-    running = True
+
+    #Put all the rows into a matrix
+
+    #SAMPLE TABLE
+    routingTable = np.array([[ '201.123.32.0', '*', '255.255.254.0', 0, 'eth1' ]])
+    routingTable = np.append(routingTable, [[ '201.123.64.0', '123.122.0.2', '255.255.192.0', 1, 'eth2']], axis=0)
+    routingTable = np.append(routingTable, [[ '123.123.123.123', '123.123.0.2', '255.255.192.0', 0, 'eth3']], axis=0)
+    routingTable = np.append(routingTable, [[ '123.123.123.123', '*', '255.255.248.0', 0, 'eth4']], axis=0)
     
+    #Sort routing table by mask length (netmaskInt(netmask))
+    routingTable = sortTable(routingTable)
 
-routerStart()
+    #Print routing table
+    print('ROUTING TABLE: ')
+    for x in range(len(routingTable)):
+        print('\t', routingTable[x])
 
+    print('')
+    return routingTable
+
+routingTable = routerStart()
 input_address = input("Enter Address: ")
 running = True
 
 while running:
+    #LOGIC: Read in address, find the right route to send it through, print
+    destination_address = findRoute(input_address, routingTable)
     route(input_address, '255.255.255.0')
+
     close = input("Route another packet? (yes/no) ")
     if(close == "no"):
         running = False
